a464b39dee9b743ffb219353b60978493415bd88
John <connollyjohnf@gmail.com>
2019-02-11 20:16:23 +0000

    broke data chunks and procedurally added them to a variable

--- stepTwo.js
+++ stepTwo.js
@@ -1,10 +1,31 @@
 function getAndPrintHTML() {
 
+
+    //eastablishes which domain and path we're requesting
     var requestOptions = {
         host: 'sytantris.github.io',
         path: '/http-examples/step2.html'
     };
+    // creates a character limit to the incoming data
+    var buf = new ArrayBuffer(255);
 
-    /* Add your code here */
+    // requires https object (built-in to the system)
+    var https = require('https');
+    var fs = require('fs');
 
+    var results = '';
+
+    // called by https when the request is made
+    function callback(response) {
+        response.on('data', function (chunk) {
+            var breaks = '';
+            breaks += (chunk.toString());
+            console.log(breaks);
+        })
+    }
+
+    // marks the end of the file
+    https.request(requestOptions, callback).end();
 }
+
+console.log(getAndPrintHTML());
