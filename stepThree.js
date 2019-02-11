7aea3728661d0150a4a48e835fd738aab9672921
John <connollyjohnf@gmail.com>
2019-02-11 20:31:19 +0000

    initial commit

---
+++ stepThree.js
@@ -0,0 +1,12 @@
+function getAndPrintHTML(options) {
+    var https = require('https');
+
+    /* Add your code here */
+
+}
+
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+};
