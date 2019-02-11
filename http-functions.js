9bcd2352eb4a89080a756e4a532c0c7410677890
John <connollyjohnf@gmail.com>
2019-02-11 22:34:54 +0000

    linked StepFive and HTTP-functions to each other

--- http-functions.js
+++ http-functions.js
@@ -1,32 +1,23 @@
-module.exports = function getHTML(options, callback) {
-  function getHTML(options, callback) {
-    var https = require("https");
+module.exports = function getHTML(requestOptions, callback) {
+  var https = require("https");
 
-    var options = {};
-    options.host = requestOptions.host;
-    options.path = requestOptions.path;
+  var options = {};
+  options.host = requestOptions.host;
+  options.path = requestOptions.path;
 
-    function callback(response) {
-      var breaks = "";
-      response.on("data", function(chunk) {
-        breaks += chunk.toString();
-      });
-      response.on("end", function() {
-        printHTML(breaks);
-      });
-    }
-    // marks the end of the file
-    https.request(options, callback).end();
+  function callback(response) {
+    var breaks = "";
+    response.on("data", function(chunk) {
+      breaks += chunk.toString();
+    });
+    response.on("end", function() {
+      printHTML(breaks);
+    });
   }
-
-  function printHTML(html) {
-    console.log(html);
-  }
-
-  var requestOptions = {
-    host: "sytantris.github.io",
-    path: "/http-examples/step4.html"
-  };
-
-  getHTML();
+  // marks the end of the file
+  https.request(options, callback).end();
 };
+
+function printHTML(html) {
+  console.log(html);
+}
--- stepFour.js
+++ stepFour.js
@@ -1,32 +1,30 @@
 function getHTML(options, callback) {
+  var https = require("https");
 
-    var https = require('https');
+  var options = {};
+  options.host = requestOptions.host;
+  options.path = requestOptions.path;
 
-    var options = {};
-    options.host = requestOptions.host;
-    options.path = requestOptions.path;
-
-    function callback(response) {
-        var breaks = '';
-        response.on('data', function (chunk) {
-            breaks += (chunk.toString());
-        })
-        response.on('end', function () {
-            printHTML(breaks);
-        });
-    }
-    // marks the end of the file
-    https.request(options, callback).end();
-
+  function callback(response) {
+    var breaks = "";
+    response.on("data", function(chunk) {
+      breaks += chunk.toString();
+    });
+    response.on("end", function() {
+      printHTML(breaks);
+    });
+  }
+  // marks the end of the file
+  https.request(options, callback).end();
 }
 
 function printHTML(html) {
-    console.log(html);
+  console.log(html);
 }
 
 var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step4.html'
+  host: "sytantris.github.io",
+  path: "/http-examples/step4.html"
 };
 
 getHTML();
