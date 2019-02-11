08a1e8f9db905713daa70175a79c329bac546a9b
John <connollyjohnf@gmail.com>
2019-02-11 21:44:55 +0000

    printing chunks via parameters

--- stepThree.js
+++ stepThree.js
@@ -1,12 +1,26 @@
 function getAndPrintHTML(options) {
     var https = require('https');
 
-    /* Add your code here */
+    var options = {};
+    options.host = requestOptions.host;
+    options.path = requestOptions.path;
 
+    function callback(response) {
+        var breaks = '';
+        response.on('data', function (chunk) {
+            breaks += (chunk.toString());
+        })
+        response.on('end', function () {
+            console.log(breaks);
+        });
+    }
+    // marks the end of the file
+    https.request(options, callback).end();
 }
-
 
 var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step3.html'
 };
+
+console.log(getAndPrintHTML())
