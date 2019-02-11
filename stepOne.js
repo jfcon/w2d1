13d7c12a85b74a3f8b18aac7e4a2f8e1437afb17
John <connollyjohnf@gmail.com>
2019-02-11 19:25:02 +0000

    renamed files to Step one and Two

---
+++ stepOne.js
@@ -0,0 +1,20 @@
+function getAndPrintHTMLChunks() {
+
+    // requires https object (built-in to the system)
+    var https = require('https');
+
+    //eastablishes which domain and path we're requesting
+    var requestOptions = {
+        host: 'sytantris.github.io',
+        path: '/http-examples/step1.html'
+    };
+
+    // called by https when the request is made
+    function callback(response) {
+            console.log(response)
+        
+    }
+    https.request(requestOptions, callback).end();
+}
+
+console.log(getAndPrintHTMLChunks());
